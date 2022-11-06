9558fd846b47c05ac0a1573a53b1ce8baa3520dd
Brian Schwind <brianmschwind@gmail.com>
2022-11-06 18:17:50 +0900

    Disable various third-party libraries

--- crates/opencascade-sys/build.rs
+++ crates/opencascade-sys/build.rs
@@ -2,6 +2,16 @@ fn main() {
     let dst = cmake::Config::new("OCCT")
         .define("BUILD_LIBRARY_TYPE", "Static")
         .define("BUILD_MODULE_Draw", "FALSE")
+        .define("USE_OPENGL", "FALSE")
+        .define("USE_GLES2", "FALSE")
+        .define("USE_D3D", "FALSE")
+        .define("USE_VTK", "FALSE")
+        .define("USE_TCL", "FALSE")
+        .define("USE_XLIB", "FALSE")
+        .define("USE_FREETYPE", "FALSE")
+        .define("USE_FREEIMAGE", "FALSE")
+        .define("USE_OPENVR", "FALSE")
+        .define("USE_FFMPEG", "FALSE")
         .build();
 
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
