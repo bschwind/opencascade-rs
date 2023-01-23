ac6d71573780081940140e6faf26f34a954bfb81
Brian Schwind <brianmschwind@gmail.com>
2023-01-23 23:38:12 +0900

    Add a basic extrude_polygon() function

--- crates/opencascade/src/lib.rs
+++ crates/opencascade/src/lib.rs
@@ -1,11 +1,14 @@
 use cxx::UniquePtr;
 use glam::DVec3;
 use opencascade_sys::ffi::{
-    gp_Ax2_ctor, gp_DZ, new_point, new_shape, write_stl, BRepAlgoAPI_Common_ctor,
-    BRepAlgoAPI_Cut_ctor, BRepAlgoAPI_Fuse_ctor, BRepFilletAPI_MakeChamfer_ctor,
-    BRepFilletAPI_MakeFillet_ctor, BRepMesh_IncrementalMesh_ctor, BRepPrimAPI_MakeBox_ctor,
-    BRepPrimAPI_MakeCylinder_ctor, StlAPI_Writer_ctor, TopAbs_ShapeEnum, TopExp_Explorer_ctor,
-    TopoDS_Shape, TopoDS_Shape_to_owned, TopoDS_cast_to_edge,
+    gp_Ax2_ctor, gp_DZ, new_HandleGeomCurve_from_HandleGeom_TrimmedCurve, new_point, new_shape,
+    new_vec, write_stl, BRepAlgoAPI_Common_ctor, BRepAlgoAPI_Cut_ctor, BRepAlgoAPI_Fuse_ctor,
+    BRepBuilderAPI_MakeEdge_HandleGeomCurve, BRepBuilderAPI_MakeFace_wire, BRepBuilderAPI_MakeWire,
+    BRepBuilderAPI_MakeWire_ctor, BRepFilletAPI_MakeChamfer_ctor, BRepFilletAPI_MakeFillet_ctor,
+    BRepMesh_IncrementalMesh_ctor, BRepPrimAPI_MakeBox_ctor, BRepPrimAPI_MakeCylinder_ctor,
+    BRepPrimAPI_MakePrism_ctor, GC_MakeSegment_Value, GC_MakeSegment_point_point,
+    StlAPI_Writer_ctor, TopAbs_ShapeEnum, TopExp_Explorer_ctor, TopoDS_Shape,
+    TopoDS_Shape_to_owned, TopoDS_cast_to_edge,
 };
 use std::path::Path;
 
@@ -47,6 +50,46 @@ impl Shape {
 
         let mut cylinder = BRepPrimAPI_MakeCylinder_ctor(&cylinder_coord_system, r, h);
         let shape = new_shape(cylinder.pin_mut().Shape());
+
+        Self { shape }
+    }
+
+    /// Purposefully underpowered for now, this simply takes a list of points,
+    /// creates a face out of them, and then extrudes it by h in the positive Z
+    /// direction.
+    pub fn extrude_polygon(points: &[DVec3], h: f64) -> Shape {
+        assert!(points.len() >= 3);
+
+        let mut make_wire = BRepBuilderAPI_MakeWire_ctor();
+
+        let add_segment =
+            |p1: DVec3, p2: DVec3, make_wire: &mut UniquePtr<BRepBuilderAPI_MakeWire>| {
+                let p1 = new_point(p1.x, p1.y, p1.z);
+                let p2 = new_point(p2.x, p2.y, p2.z);
+
+                let segment = GC_MakeSegment_point_point(&p1, &p2);
+                let mut edge = BRepBuilderAPI_MakeEdge_HandleGeomCurve(
+                    &new_HandleGeomCurve_from_HandleGeom_TrimmedCurve(&GC_MakeSegment_Value(
+                        &segment,
+                    )),
+                );
+
+                make_wire.pin_mut().add_edge(edge.pin_mut().Edge());
+            };
+
+        for window in points.windows(2) {
+            add_segment(window[0], window[1], &mut make_wire);
+        }
+
+        add_segment(*points.last().unwrap(), points[0], &mut make_wire);
+
+        let wire_profile = make_wire.pin_mut().Wire();
+        let mut face_profile = BRepBuilderAPI_MakeFace_wire(wire_profile, false);
+        let prism_vec = new_vec(0.0, 0.0, h);
+        let mut extrusion =
+            BRepPrimAPI_MakePrism_ctor(face_profile.pin_mut().Shape(), &prism_vec, false, true);
+
+        let shape = new_shape(extrusion.pin_mut().Shape());
 
         Self { shape }
     }
