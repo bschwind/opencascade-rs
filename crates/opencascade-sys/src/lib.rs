7fdd3096f1fb14090f0ffaa0dc996e66dc5f695c
Dominick Schroer <dominick@schroer.ca>
2023-01-24 07:57:45 -0700

    Remove unneeded safety comment

--- crates/opencascade-sys/src/lib.rs
+++ crates/opencascade-sys/src/lib.rs
@@ -455,9 +455,6 @@ pub mod ffi {
 
         type Handle_Poly_Triangulation;
         pub fn IsNull(self: &Handle_Poly_Triangulation) -> bool;
-        /// # Safety
-        /// Since this handle may be null, this is only safe if IsNull has been checked.
-        /// The handle must also outlive the reference returned.
         pub fn get(self: &Handle_Poly_Triangulation) -> *mut Poly_Triangulation;
 
         type Poly_Triangulation;
