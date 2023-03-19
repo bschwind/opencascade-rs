df125ab2e8b651f4fbdf150eca0f40573f95bddd
Brian Schwind <brianmschwind@gmail.com>
2023-03-19 21:51:02 +0900

    Make the high_level example easier to distinguish orientation

--- crates/opencascade/examples/high_level.rs
+++ crates/opencascade/examples/high_level.rs
@@ -3,7 +3,7 @@ use opencascade::geometry::Workspace;
 pub fn main() {
     let mut workspace = Workspace::new();
 
-    let extruded_box = workspace.sketch().rect(16.0, 9.0).extrude(7.0);
+    let extruded_box = workspace.sketch().rect(16.0, 9.0).extrude(2.0);
     workspace.add(extruded_box);
 
     workspace.write_stl("high_level.stl").unwrap();
