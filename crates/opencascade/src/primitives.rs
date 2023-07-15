3a8eb2a246ec9773098b65c743cbdfb9a781994c
Brian Schwind <brianmschwind@gmail.com>
2023-07-15 23:09:46 +0900

    Add explanatory comment for all the AsRef impls

--- crates/opencascade/src/primitives.rs
+++ crates/opencascade/src/primitives.rs
@@ -71,6 +71,16 @@ pub struct Vertex {
     _inner: UniquePtr<ffi::TopoDS_Vertex>,
 }
 
+// You'll see several of these `impl AsRef` blocks for the various primitive
+// geometry types. This is for functions which take an Iterator of primitives
+// which are either owned or borrowed values. The general pattern looks like this:
+//
+//     pub fn do_something_with_edges<T: AsRef<Edge>>(edges: impl IntoIterator<Item = T>) {
+//         for edge in edges.into_iter() {
+//             let edge_ref = edge.as_ref();
+//             // Do something with edge_ref
+//         }
+//     }
 impl AsRef<Vertex> for Vertex {
     fn as_ref(&self) -> &Vertex {
         self
