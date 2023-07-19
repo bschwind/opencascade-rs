cecb3d0ed9cc9822651436a3277ac379f7be7570
Brian Schwind <brianmschwind@gmail.com>
2023-07-19 12:19:49 +0900

    Start work on excluding unnecessary CMake modules

--- crates/occt-sys/build.rs
+++ crates/occt-sys/build.rs
@@ -2,19 +2,31 @@ const LIB_DIR: &str = "lib";
 const INCLUDE_DIR: &str = "include";
 
 fn main() {
+    let current_dir = std::env::current_dir().expect("Should have a 'current' directory");
+    let patch_dir = current_dir.join("patch");
+
     let dst = cmake::Config::new("OCCT")
+        // TODO(bschwind) - Verify this is set properly
+        // .define("CMAKE_BUILD_TYPE", "Release")
+        .define("BUILD_PATCH", patch_dir)
         .define("BUILD_LIBRARY_TYPE", "Static")
         .define("BUILD_MODULE_Draw", "FALSE")
-        .define("USE_OPENGL", "FALSE")
-        .define("USE_GLES2", "FALSE")
         .define("USE_D3D", "FALSE")
-        .define("USE_VTK", "FALSE")
-        .define("USE_TCL", "FALSE")
-        .define("USE_XLIB", "FALSE")
-        .define("USE_FREETYPE", "FALSE")
-        .define("USE_FREEIMAGE", "FALSE")
-        .define("USE_OPENVR", "FALSE")
+        .define("USE_DRACO", "FALSE")
+        .define("USE_EIGEN", "FALSE")
         .define("USE_FFMPEG", "FALSE")
+        .define("USE_FREEIMAGE", "FALSE")
+        .define("USE_FREETYPE", "FALSE")
+        .define("USE_GLES2", "FALSE")
+        .define("USE_OPENGL", "FALSE")
+        .define("USE_OPENVR", "FALSE")
+        .define("USE_RAPIDJSON", "FALSE")
+        .define("USE_TBB", "FALSE")
+        .define("USE_TCL", "FALSE")
+        .define("USE_TK", "FALSE")
+        .define("USE_VTK", "FALSE")
+        .define("USE_XLIB", "FALSE")
+        .define("USE_XLIB", "FALSE")
         .define("INSTALL_DIR_LIB", LIB_DIR)
         .define("INSTALL_DIR_INCLUDE", INCLUDE_DIR)
         .build();
