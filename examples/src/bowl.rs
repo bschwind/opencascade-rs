0dd22bf5394b45214ffe34308f0465796feab195
Simon Kirkby <obeygiantrobot@gmail.com>
2023-07-25 07:36:52 +0800

    Fix formatting and trailing zeros

--- examples/src/bowl.rs
+++ examples/src/bowl.rs
@@ -1,6 +1,6 @@
 use glam::dvec3;
 use opencascade::{
-    primitives::{self, Direction,  Solid},
+    primitives::{self, Direction, Solid},
     workplane::Workplane,
 };
 
@@ -10,7 +10,7 @@ pub fn main() {
     let height: f64 = 30.0;
     let thickness: f64 = 2.0;
     // the base bowl
-    let mut loft = bowly_shape(bot_rad, top_rad, height, 0.);
+    let mut loft = bowly_shape(bot_rad, top_rad, height, 0.0);
 
     // inner ( shell does not exist yest)
     let inner = bowly_shape(bot_rad - thickness, top_rad - thickness, height, thickness);
@@ -24,14 +24,14 @@ pub fn main() {
 }
 
 fn bowly_shape(bot_rad: f64, top_rad: f64, height: f64, offset: f64) -> primitives::Shape {
-    let bottom = Workplane::xy().circle(0., 0., bot_rad);
-    let mut top = Workplane::xy().circle(0., 0., top_rad);
-    top.translate(dvec3(0., 0., height));
+    let bottom = Workplane::xy().circle(0.0, 0.0, bot_rad);
+    let mut top = Workplane::xy().circle(0.0, 0.0, top_rad);
+    top.translate(dvec3(0.0, 0.0, height));
     let mut loft = Solid::loft([&bottom, &top]).to_shape();
-    loft.set_global_translation(dvec3(0., 0., offset));
+    loft.set_global_translation(dvec3(0.0, 0.0, offset));
     // round out the bottom
     let bottom_side = loft.faces().farthest(Direction::NegZ).edges();
-    loft.fillet_edges(4., bottom_side);
+    loft.fillet_edges(4.0, bottom_side);
 
     loft
 }
