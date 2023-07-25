2b804aa26d273aedcd5c29b18818fe6b0296b49a
Julian Schuler <julianschuler@users.noreply.github.com>
2023-07-25 11:51:10 +0200

    Scale up airfoil example

--- examples/src/airfoil.rs
+++ examples/src/airfoil.rs
@@ -6,23 +6,23 @@ use opencascade::primitives::{Face, Shape, Surface};
 pub fn shape() -> Shape {
     let points1 = [
         dvec3(0.0, 0.0, 0.0),
-        dvec3(0.5, 0.0, 1.0),
-        dvec3(2.0, 0.0, 1.0),
-        dvec3(5.0, 0.0, 1.0),
-        dvec3(10.0, 0.0, 0.0),
+        dvec3(5.0, 0.0, 10.0),
+        dvec3(20.0, 0.0, 10.0),
+        dvec3(50.0, 0.0, 10.0),
+        dvec3(100.0, 0.0, 0.0),
     ];
     let points2 = [
         dvec3(0.0, 0.0, 0.0),
-        dvec3(-0.2, 0.0, -0.8),
-        dvec3(5.0, 0.0, 0.0),
-        dvec3(5.0, 0.0, 0.0),
-        dvec3(6.0, 0.0, 0.0),
+        dvec3(-2.0, 0.0, -8.0),
+        dvec3(50.0, 0.0, 0.0),
+        dvec3(50.0, 0.0, 0.0),
+        dvec3(100.0, 0.0, 0.0),
     ];
 
     let surface = Surface::bezier(&[&points1, &points2]);
     let face = Face::from_surface(&surface);
 
-    let airfoil = face.extrude(dvec3(0.0, 10.0, 0.0));
+    let airfoil = face.extrude(dvec3(0.0, 50.0, 0.0));
 
     airfoil.into()
 }
