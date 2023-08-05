3138a193868183f98042e4cc969eb6ed5e06db0b
Julian Schuler <julianschuler@users.noreply.github.com>
2023-08-05 22:24:18 +0200

    Add from_shapes method for coupound

--- crates/opencascade/src/primitives/compound.rs
+++ crates/opencascade/src/primitives/compound.rs
@@ -22,4 +22,21 @@ impl Compound {
 
         shape
     }
+
+    pub fn from_shapes<T: AsRef<Shape>>(shapes: impl IntoIterator<Item = T>) -> Self {
+        let mut compound = ffi::TopoDS_Compound_ctor();
+        let builder = ffi::BRep_Builder_ctor();
+        let builder = ffi::BRep_Builder_upcast_to_topods_builder(&builder);
+        builder.MakeCompound(compound.pin_mut());
+        let mut compound_shape = ffi::TopoDS_Compound_as_shape(compound);
+
+        for shape in shapes.into_iter() {
+            builder.Add(compound_shape.pin_mut(), &shape.as_ref().inner);
+        }
+
+        let inner = ffi::TopoDS_cast_to_compound(&compound_shape);
+        let inner = ffi::TopoDS_Compound_to_owned(inner);
+
+        Self { inner }
+    }
 }
