2cf5898406a5bdd16a46d57e9b00466668b27781
Brian Schwind <brianmschwind@gmail.com>
2023-09-03 16:15:11 +0900

    Update the variable-fillet example to the new chained call API

--- examples/src/variable_fillet.rs
+++ examples/src/variable_fillet.rs
@@ -8,13 +8,11 @@ use opencascade::{
 pub fn shape() -> Shape {
     let base = Workplane::xy().rect(50.0, 50.0);
     let shape = base.to_face().extrude(dvec3(0.0, 0.0, 50.0));
-    let mut shape = shape.into_shape();
+    let shape = shape.into_shape();
 
     let right_edge = shape.faces().farthest(Direction::PosX).edges().next().unwrap();
     shape.variable_fillet_edge(
         [(0.0, 7.0), (0.2, 20.0), (0.5, 3.0), (0.8, 20.0), (1.0, 7.0)],
         &right_edge,
-    );
-
-    shape
+    )
 }
