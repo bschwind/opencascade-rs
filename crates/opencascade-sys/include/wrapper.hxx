cc6c16938761c888db56c9a65e981e907cb7ef7f
Mate Kovacs <mate@braid.tech>
2023-10-16 18:41:16 +0900

    add Shell::loft(wires) and underlying machinery

--- crates/opencascade-sys/include/wrapper.hxx
+++ crates/opencascade-sys/include/wrapper.hxx
@@ -232,6 +232,7 @@ inline const TopoDS_Vertex &TopoDS_cast_to_vertex(const TopoDS_Shape &shape) { r
 inline const TopoDS_Edge &TopoDS_cast_to_edge(const TopoDS_Shape &shape) { return TopoDS::Edge(shape); }
 inline const TopoDS_Wire &TopoDS_cast_to_wire(const TopoDS_Shape &shape) { return TopoDS::Wire(shape); }
 inline const TopoDS_Face &TopoDS_cast_to_face(const TopoDS_Shape &shape) { return TopoDS::Face(shape); }
+inline const TopoDS_Shell &TopoDS_cast_to_shell(const TopoDS_Shape &shape) { return TopoDS::Shell(shape); }
 inline const TopoDS_Solid &TopoDS_cast_to_solid(const TopoDS_Shape &shape) { return TopoDS::Solid(shape); }
 inline const TopoDS_Compound &TopoDS_cast_to_compound(const TopoDS_Shape &shape) { return TopoDS::Compound(shape); }
 
@@ -239,6 +240,7 @@ inline const TopoDS_Shape &cast_vertex_to_shape(const TopoDS_Vertex &vertex) { r
 inline const TopoDS_Shape &cast_edge_to_shape(const TopoDS_Edge &edge) { return edge; }
 inline const TopoDS_Shape &cast_wire_to_shape(const TopoDS_Wire &wire) { return wire; }
 inline const TopoDS_Shape &cast_face_to_shape(const TopoDS_Face &face) { return face; }
+inline const TopoDS_Shape &cast_shell_to_shape(const TopoDS_Shell &shell) { return shell; }
 inline const TopoDS_Shape &cast_solid_to_shape(const TopoDS_Solid &solid) { return solid; }
 inline const TopoDS_Shape &cast_compound_to_shape(const TopoDS_Compound &compound) { return compound; }
 
