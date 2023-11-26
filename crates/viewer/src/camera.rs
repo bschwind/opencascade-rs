9a3763bef4ac51be37e775a14fcd339b75a46f89
Mate Kovacs <mkovaxx@gmail.com>
2023-11-26 21:58:38 +0900

    make zooming work in ortho mode

--- crates/viewer/src/camera.rs
+++ crates/viewer/src/camera.rs
@@ -59,7 +59,7 @@ impl OrbitCamera {
 
     /// Zoom in or out, while looking at the same target.
     pub fn zoom(&mut self, zoom_delta: f32) {
-        self.radius = f32::max(self.radius * f32::exp(zoom_delta), f32::EPSILON);
+        self.zoom_factor = f32::max(self.zoom_factor * f32::exp(zoom_delta), f32::EPSILON);
     }
 
     /// Orbit around the target while keeping the distance.
@@ -71,23 +71,33 @@ impl OrbitCamera {
         // These magic numbers are configured so that the particular model we are loading is
         // visible in its entirety. They will be dynamically computed eventually when we have "fit
         // to view" function or alike.
-        let proj = match self.projection {
-            Projection::Orthographic => Mat4::orthographic_rh(
-                -50.0 * self.zoom_factor * self.aspect_ratio,
-                50.0 * self.zoom_factor * self.aspect_ratio,
-                -50.0 * self.zoom_factor,
-                50.0 * self.zoom_factor,
-                -1000.0,
-                1000.0,
-            ),
+
+        let (proj, effective_radius) = match self.projection {
+            Projection::Orthographic => {
+                let proj = Mat4::orthographic_rh(
+                    -50.0 * self.zoom_factor * self.aspect_ratio,
+                    50.0 * self.zoom_factor * self.aspect_ratio,
+                    -50.0 * self.zoom_factor,
+                    50.0 * self.zoom_factor,
+                    -1000.0,
+                    1000.0,
+                );
+                (proj, self.radius)
+            },
             Projection::Perspective => {
-                Mat4::perspective_rh(std::f32::consts::PI / 2.0, self.aspect_ratio, 0.01, 1000.0)
+                let proj = Mat4::perspective_rh(
+                    std::f32::consts::PI / 2.0,
+                    self.aspect_ratio,
+                    0.01,
+                    1000.0,
+                );
+                (proj, self.zoom_factor * self.radius)
             },
         };
 
         let local_frame = self.get_local_frame();
-        let position = self.target + self.radius * local_frame.z_axis;
         let upward = local_frame.y_axis;
+        let position = self.target + effective_radius * local_frame.z_axis;
         let view = Mat4::look_at_rh(position, self.target, upward);
 
         proj * view
