d881779cd6e368492a34d79a48f5d596bb06b58a
Mate Kovacs <mkovaxx@gmail.com>
2024-06-06 17:02:55 +0900

    cleanup & multi-format

---
+++ examples/src/write_model.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+use examples::Example;
+
+/// Save an example model to a file
+#[derive(Debug, Clone, Parser)]
+struct Args {
+    /// Example to save
+    example: Example,
+
+    /// Output file path
+    #[clap(short, long, default_value = "output.step")]
+    output: PathBuf,
+
+    /// Output format
+    #[clap(short, long, default_value = "step")]
+    format: Format,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum Format {
+    Step,
+    Stl,
+}
+
+fn main() {
+    let args = Args::parse();
+    let model = args.example.shape();
+    match args.format {
+        Format::Step => model.write_step(&args.output).unwrap(),
+        Format::Stl => model.write_stl(&args.output).unwrap(),
+    }
+}
