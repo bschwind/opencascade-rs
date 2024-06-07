09dd736f3a1e203721781755ab74632228d62c26
Mate Kovacs <mkovaxx@gmail.com>
2024-06-07 22:20:13 +0900

    cleanup

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -12,8 +12,8 @@ pub mod keycap;
 pub mod offset_2d;
 pub mod rounded_chamfer;
 pub mod swept_face;
-pub mod swept_face_variable;
 pub mod swept_wire;
+pub mod swept_wire_variable;
 pub mod turners_cube;
 pub mod variable_fillet;
 
@@ -30,8 +30,8 @@ pub enum Example {
     Offset2d,
     RoundedChamfer,
     SweptFace,
-    SweptFaceVariable,
     SweptWire,
+    SweptWireVariable,
     TurnersCube,
     VariableFillet,
 }
@@ -50,8 +50,8 @@ impl Example {
             Example::Offset2d => offset_2d::shape(),
             Example::RoundedChamfer => rounded_chamfer::shape(),
             Example::SweptFace => swept_face::shape(),
-            Example::SweptFaceVariable => swept_face_variable::shape(),
             Example::SweptWire => swept_wire::shape(),
+            Example::SweptWireVariable => swept_wire_variable::shape(),
             Example::TurnersCube => turners_cube::shape(),
             Example::VariableFillet => variable_fillet::shape(),
         }
