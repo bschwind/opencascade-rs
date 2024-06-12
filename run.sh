bb39e104fa7d3baa15c31beeede30956ba194d27
Brian Schwind <brianmschwind@gmail.com>
2024-06-12 23:28:14 +0900

    Remove unnecessary run.sh script

--- run.sh
+++
@@ -1,3 +0,0 @@
-cargo build -p wasm-example --release --target wasm32-unknown-unknown
-# wasm-tools component new ./target/wasm32-unknown-unknown/release/wasm_example.wasm -o my-component.wasm
-cargo run --release --no-default-features --bin viewer
