eb21a7d45b8a562c367c9170668956851c525801
Brian Schwind <brianmschwind@gmail.com>
2024-09-15 19:16:51 +0900

    Attach the feet to the case

--- examples/src/keyboard_case.rs
+++ examples/src/keyboard_case.rs
@@ -304,8 +304,11 @@ fn case_foot(center: DVec2, pointing_down: bool) -> Shape {
     const HALF_FOOT_THICKNESS: f64 = FOOT_THICKNESS / 2.0;
     const FOOT_EXTENT: f64 = 15.0;
 
+    let mut workplane = Workplane::xy();
+    workplane.set_translation(dvec3(0.0, 0.0, CASE_BOTTOM_Z));
+
     let sketch = if pointing_down {
-        Workplane::xy()
+        workplane
             .sketch()
             .move_to(center.x - HALF_FOOT_THICKNESS, center.y - HALF_FOOT_THICKNESS)
             .line_dx(-FOOT_EXTENT)
@@ -317,7 +320,7 @@ fn case_foot(center: DVec2, pointing_down: bool) -> Shape {
             .line_dx(-FOOT_THICKNESS)
             .close()
     } else {
-        Workplane::xy()
+        workplane
             .sketch()
             .move_to(center.x + HALF_FOOT_THICKNESS, center.y + HALF_FOOT_THICKNESS)
             .line_dx(FOOT_EXTENT)
@@ -331,6 +334,23 @@ fn case_foot(center: DVec2, pointing_down: bool) -> Shape {
     };
 
     sketch.fillet(0.7).to_face().extrude(dvec3(0.0, 0.0, -5.0)).into()
+}
+
+fn case_feet() -> Shape {
+    let reference_point = dvec2(PCB_WIDTH / 2.0, ((CASE_BOTTOM - CASE_TOP) / 2.0) + 0.8);
+    let upper_left_foot_pos = reference_point + dvec2(-90.0, 18.5);
+    let bottom_left_foot_pos = upper_left_foot_pos + dvec2(14.0, -55.3);
+
+    let upper_left_foot = case_foot(upper_left_foot_pos, true);
+    let upper_right_foot = case_foot(upper_left_foot_pos + dvec2(171.1, 0.0), true);
+    let bottom_left_foot = case_foot(bottom_left_foot_pos, false);
+    let bottom_right_foot = case_foot(bottom_left_foot_pos + dvec2(152.1, 0.0), false);
+
+    upper_left_foot
+        .union(&upper_right_foot)
+        .union(&bottom_left_foot)
+        .union(&bottom_right_foot)
+        .into()
 }
 
 pub fn shape() -> Shape {
@@ -390,34 +410,11 @@ pub fn shape() -> Shape {
 
     // shape.write_stl("keyboard_half.stl").unwrap();
 
-    // case.write_step("keyboard.step").unwrap();
+    let feet = case_feet();
 
-    // case
-    // let foot = case_foot(dvec2(0.0, 0.0), pointing_down);
+    let case = case.union(&feet).into_shape();
 
-    let pcb_center = dvec2(PCB_WIDTH / 2.0, PCB_HEIGHT / 2.0);
+    case.write_step("keyboard.step").unwrap();
 
-    let upper_left_foot_pos = pcb_center + dvec2(-90.0, 18.5);
-    let bottom_left_foot_pos = upper_left_foot_pos + dvec2(14.0, -55.3);
-
-    let upper_left_foot = case_foot(upper_left_foot_pos, true);
-    let upper_right_foot = case_foot(upper_left_foot_pos + dvec2(171.1, 0.0), true);
-    let bottom_left_foot = case_foot(bottom_left_foot_pos, false);
-    let bottom_right_foot = case_foot(bottom_left_foot_pos + dvec2(152.1, 0.0), false);
-
-    // Temporary plate to hold the feet
-    let corner_1 = DVec3::new(pcb_center.x - 120.0, pcb_center.y - 35.0 - 10.0, 0.0);
-    let corner_2 = DVec3::new(pcb_center.x + 120.0, pcb_center.y + 35.0 - 10.0, 0.5);
-    let plate = Shape::box_from_corners(corner_1, corner_2);
-
-    let feet = upper_left_foot
-        .union(&upper_right_foot)
-        .union(&bottom_left_foot)
-        .union(&bottom_right_foot)
-        .union(&plate)
-        .into_shape();
-
-    feet.write_step("keyboard_case_feet_plate_test.step").unwrap();
-
-    feet
+    case
 }
