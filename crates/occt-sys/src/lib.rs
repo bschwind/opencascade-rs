c754f4fb64d90285a5b6880102e87920e7ce3f25
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-11-30 14:01:50 +0100

    fix: allow cross compilation under builtin flag v2 (#185)
    * fix: allow cross compilation under builtin flag
    
    * undo sys changes
    
    ---------
    
    Co-authored-by: Dominick Schroer <dominick@schroer.ca>

--- crates/occt-sys/src/lib.rs
+++ crates/occt-sys/src/lib.rs
@@ -1,9 +1,46 @@
-use std::path::Path;
+use std::{
+    env::var,
+    path::{Path, PathBuf},
+};
+
+const LIB_DIR: &str = "lib";
+const INCLUDE_DIR: &str = "include";
 
 /// Get the path to the OCCT library installation directory to be
 /// used in build scripts.
 ///
 /// Only valid during build (`cargo clean` removes these files).
-pub fn occt_path() -> &'static Path {
-    Path::new(env!("OCCT_PATH"))
+pub fn occt_path() -> PathBuf {
+    // moves the output into target/TARGET/OCCT
+    // this way its less likely to be rebuilt without a cargo clean
+    Path::new(&var("OUT_DIR").expect("missing OUT_DIR")).join("../../../../OCCT")
+}
+
+/// Build the OCCT library.
+pub fn build_occt() {
+    cmake::Config::new(Path::new(env!("OCCT_SRC_DIR")))
+        .define("BUILD_PATCH", Path::new(env!("OCCT_PATCH_DIR")))
+        .define("BUILD_LIBRARY_TYPE", "Static")
+        .define("BUILD_MODULE_ApplicationFramework", "FALSE")
+        .define("BUILD_MODULE_Draw", "FALSE")
+        .define("USE_D3D", "FALSE")
+        .define("USE_DRACO", "FALSE")
+        .define("USE_EIGEN", "FALSE")
+        .define("USE_FFMPEG", "FALSE")
+        .define("USE_FREEIMAGE", "FALSE")
+        .define("USE_FREETYPE", "FALSE")
+        .define("USE_GLES2", "FALSE")
+        .define("USE_OPENGL", "FALSE")
+        .define("USE_OPENVR", "FALSE")
+        .define("USE_RAPIDJSON", "FALSE")
+        .define("USE_TBB", "FALSE")
+        .define("USE_TCL", "FALSE")
+        .define("USE_TK", "FALSE")
+        .define("USE_VTK", "FALSE")
+        .define("USE_XLIB", "FALSE")
+        .define("INSTALL_DIR_LIB", LIB_DIR)
+        .define("INSTALL_DIR_INCLUDE", INCLUDE_DIR)
+        .profile("Release")
+        .out_dir(occt_path())
+        .build();
 }
