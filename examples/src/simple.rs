fd23d92c795fdb5eec06af0ecf2594f2395089bb
Tyler Kvochick <tyler.kvochick@teecom.com>
2025-05-06 10:47:10 -0400

    Show compound shape

--- examples/src/simple.rs
+++ examples/src/simple.rs
@@ -1,11 +1,18 @@
-use glam::DVec3;
+use glam::dvec3;
 use opencascade::{
-    primitives::{IntoShape, Shape},
+    primitives::{Compound, IntoShape, Shape},
+    section::Section,
     workplane::Workplane,
 };
 
 pub fn shape() -> Shape {
     let f = Workplane::xy().rect(1., 2.).to_face();
 
-    f.extrude(DVec3::new(0., 0., 3.)).into_shape()
+    let b = f.extrude(dvec3(0., 0., 3.)).into_shape();
+
+    let p = Workplane::yz().rect(10., 10.).to_face().into_shape();
+
+    let edges = Section::new(&b, &p).section_edges();
+
+    Compound::from_shapes(vec![edges, vec![b], vec![p]].iter().flatten()).into_shape()
 }
