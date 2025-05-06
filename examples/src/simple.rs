ff47028721fdc50c05c9e064e485aca6448de01a
Tyler Kvochick <tyler.kvochick@teecom.com>
2025-05-06 13:06:09 -0400

    Use pipe as target

--- examples/src/simple.rs
+++ examples/src/simple.rs
@@ -1,18 +1,30 @@
+use core::f64;
 use glam::dvec3;
 use opencascade::{
+    angle::Angle::Radians,
     primitives::{Compound, IntoShape, Shape},
     section::Section,
     workplane::Workplane,
 };
 
 pub fn shape() -> Shape {
-    let f = Workplane::xy().rect(1., 2.).to_face();
+    let s = Workplane::xy()
+        .sketch()
+        .arc((0., -2.), (1., -1.), (0., 0.))
+        .arc((0., 0.), (-1., 1.), (0., 2.))
+        .wire();
 
-    let b = f.extrude(dvec3(0., 0., 3.)).into_shape();
+    let f = Workplane::yz().circle(0., 0., 0.5).to_face();
 
-    let p = Workplane::yz().rect(10., 10.).to_face().into_shape();
+    let shape = f.sweep_along(&s).into_shape();
 
-    let edges = Section::new(&b, &p).section_edges();
+    let p = Workplane::yz()
+        .rect(10., 10.)
+        .transform(dvec3(0., 0., 0.), dvec3(0., 0., 1.), Radians(f64::consts::PI / 6.))
+        .to_face()
+        .into_shape();
 
-    Compound::from_shapes(vec![edges, vec![b], vec![p]].iter().flatten()).into_shape()
+    let edges = Section::new(&shape, &p).section_edges();
+
+    Compound::from_shapes(vec![edges, vec![shape], vec![p]].iter().flatten()).into_shape()
 }
