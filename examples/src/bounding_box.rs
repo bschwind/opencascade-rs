42d5fb027552ab2fc148bfba56abfa8aee4fe7c2
Tyler Kvochick <tyler.kvochick@teecom.com>
2025-06-16 14:29:08 -0400

    Use both sides of decimal

--- examples/src/bounding_box.rs
+++ examples/src/bounding_box.rs
@@ -9,12 +9,12 @@ pub fn shape() -> Shape {
     for i in 0..=1 {
         // NOTE: Try changing the range values and the position vector
         let v = (2i32.pow(i)) as f64;
-        let s = Shape::sphere(1.).at(glam::dvec3(v, v, v * 2.)).build();
+        let s = Shape::sphere(1.0).at(glam::dvec3(v, v, v * 2.0)).build();
         shapes.push(s);
     }
 
     // Add a circle to show that edges work, too.
-    shapes.push(Workplane::xy().circle(0., 0., 2.).into_shape());
+    shapes.push(Workplane::xy().circle(0.0, 0.0, 2.0).into_shape());
 
     // Combine them to create a bounding box.
     let c = Compound::from_shapes(shapes);
