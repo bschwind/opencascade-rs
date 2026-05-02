e3930f983ea06b57124a81138f8042ad6a40f0ee
Brian Schwind <brianmschwind@gmail.com>
2026-05-02 23:24:40 +0900

    Simplify BRepTools binding as an example

--- crates/opencascade-sys/src/b_rep_tools.rs
+++ crates/opencascade-sys/src/b_rep_tools.rs
@@ -1,10 +1,9 @@
-use cxx::UniquePtr;
 pub use inner::*;
 
 #[cxx::bridge]
 mod inner {
     unsafe extern "C++" {
-        include!("opencascade-sys/include/wrapper.hxx");
+        include!("b_rep_tools.hxx");
 
         type TopoDS_Face = crate::ffi::TopoDS_Face;
         type TopoDS_Wire = crate::ffi::TopoDS_Wire;
@@ -12,11 +11,5 @@ mod inner {
         type BRepTools;
 
         fn outer_wire(face: &TopoDS_Face) -> UniquePtr<TopoDS_Wire>;
-    }
-}
-
-impl BRepTools {
-    pub fn outer_wire(face: &TopoDS_Face) -> UniquePtr<TopoDS_Wire> {
-        outer_wire(face)
     }
 }
