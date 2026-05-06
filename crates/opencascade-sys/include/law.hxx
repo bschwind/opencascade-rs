3c20f604b8a78d6e30bedb4312f2f9a1ab5d97de
Brian Schwind <brianmschwind@gmail.com>
2026-05-06 17:45:28 +0900

    Remove an unnecessary comment

--- crates/opencascade-sys/include/law.hxx
+++ crates/opencascade-sys/include/law.hxx
@@ -6,7 +6,6 @@ inline std::unique_ptr<Handle_Law_Function> Law_Function_to_handle(std::unique_p
   return std::unique_ptr<Handle_Law_Function>(new Handle_Law_Function(law_function.release()));
 }
 
-// Law_Interpol stuff
 inline std::unique_ptr<Law_Function> Law_Interpol_into_Law_Function(std::unique_ptr<Law_Interpol> law_interpol) {
   return std::unique_ptr<Law_Function>(law_interpol.release());
 }
